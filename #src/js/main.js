5cd4149581bf67435a0503b79671733f4b1fde7a
Dmitrii <dbjenov@gmail.com>
2021-04-27 20:03:02 +0300

    lesson 7

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,4 +1,48 @@
+$(document).ready(function ()  {
+   $('button[filter="2"]').click(function () {
+   if($(this).attr('val') == 'off') {
+      $('button[filter]').attr('val', 'off');
+      $(this).attr('val', 'on');
+      $('.filter > div ').hide(300);
+      $('.filter > div[filter="2"] ').show(300);
+   
+   }
+   
+   }); 
+   $('button[filter="3"]').click(function () {
+      if($(this).attr('val') == 'off') {
+         $('button[filter]').attr('val','off');
+         $(this).attr('val','on');
+         
+         $('.filter > div ').hide(300);
+         $('.filter > div[filter="3"] ').show(300);
+      
+      }
+      
+      }); 
+      $('button[filter="4"]').click(function () {
+         if($(this).attr('val') == 'off') {
+            $('button[filter]').attr('val', 'off');
+            $(this).attr('val', 'on');
+            
+            $('.filter > div ').hide(300);
+            $('.filter > div[filter="4"] ').show(300);
+         
+         }
+         
+         }); 
 
-console.log(1);
+         $('button[filter="1"]').click(function () {
+            if($(this).attr('val') == 'off') {
+               $('button[filter]').attr('val', 'off');
+               $(this).attr('val', 'on');
+               
+               $('.filter > div ').show(300);
+               
+            
+            }
+            
+            }); 
+});  
 
 
